23197b759bb173e960c1e7303ac9b54afb39ed0f
EAGLE-3 <EAGLE-3@EAGLE-1>
2024-02-06 14:55:45 +0100

    Aggiungere i file di progetto.

---
+++ BE-U1-W1-D3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE_U1_W1_D3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] listanomi = { "francesco", "marco", "mario", "chiara", "lorenzo" };
+            string nome;
+            int i;
+            bool verifica = false;
+
+            Console.WriteLine("Inserire il nome da ricercare");
+            nome = Console.ReadLine();
+
+            for (i = 0; i < listanomi.Length; i++)
+            {
+                if (listanomi[i] == nome)
+                {
+                    Console.WriteLine("Il nome è presente nella lista");
+                    verifica = true;
+                }
+
+            }
+            if (verifica == false)
+            {
+                Console.WriteLine("Il nome non è presente nella lista");
+            }
+
+            Console.ReadLine();
+        }
+      
+    }
+}
